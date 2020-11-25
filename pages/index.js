60074be857856f186491d3d06ab9f545e777dde9
Alex Smith <fnkcollective@hotmail.co.uk>
2020-11-25 17:37:39 +0000

    Updates index.js to redirect to /documents/Y8wDM

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,12 @@
-import { Home } from "../lib/pages";
+function Home() {
+  return null;
+}
+
+export async function getServerSideProps(context) {
+  context.res.writeHead(302, { Location: `/documents/Y8wDM` });
+  context.res.end();
+
+  return { props: {} };
+}
 
 export default Home;
