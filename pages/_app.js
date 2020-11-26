31c68ddce25d01ced1fbc1986d561267cdd90f25
Alex Smith <fnkcollective@hotmail.co.uk>
2020-11-26 14:02:35 +0000

    Adds apollo client

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,20 @@
 import { ThemeProvider } from "styled-components";
+import { ApolloProvider } from "@apollo/client";
+import { useApollo } from "../lib/gql/client";
 import { theme } from "../lib/styles/theme";
 import { GlobalStyle } from "../lib/styles/GlobalStyle";
 
 export default function App({ Component, pageProps }) {
+  const apolloClient = useApollo(pageProps);
+
   return (
     <>
       <GlobalStyle />
-      <ThemeProvider theme={theme}>
-        <Component {...pageProps} />
-      </ThemeProvider>
+      <ApolloProvider client={apolloClient}>
+        <ThemeProvider theme={theme}>
+          <Component {...pageProps} />
+        </ThemeProvider>
+      </ApolloProvider>
     </>
   );
 }
